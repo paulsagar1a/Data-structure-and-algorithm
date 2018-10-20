2bdfb819bfb9666d833181b57e849c84d803fa80
paulsagar1a <paulsagar1a@gmail.com>
2018-10-20 15:08:57 +0530

    2nd commit for linked list union and intersection

--- linkedlist/UnionAndIntersection.java
+++ linkedlist/UnionAndIntersection.java
@@ -19,7 +19,7 @@ public class UnionAndIntersection {
 	 * 	3. Follow the steps:
 	 * 		Union:
 	 * 			a) create a new list, say newList
-	 * 			b) run a loop: list1->next != null
+	 * 			b) run a loop: list1->next != null && list2->next !=null
 	 * 				if [list1->data < list2->data]
 	 * 					add list1->data to the newList
 	 * 					list1 = list1->next
@@ -27,6 +27,7 @@ public class UnionAndIntersection {
 	 * 					add list2->data to the newList
 	 * 					list2 = list2->next
 	 * 				else //when [list1->data = list2->data]
+	 * 					add list2->data to the newList
 	 * 					list1 = list1->next and
 	 * 					list2 = list2->next
 	 * 			c) In case any of the two list list->next not null i.e.
@@ -36,7 +37,7 @@ public class UnionAndIntersection {
 	 * 
 	 * 		Intersection:
 	 * 			a) create a new list, say newList
-	 * 			b) run a loop: list1->next != null
+	 * 			b) run a loop: list1->next != null && list2->next !=null
 	 * 				if [list1->data < list2->data]
 	 * 					list1 = list1->next
 	 * 				else if [list2->data < list2->data]
@@ -50,8 +51,8 @@ public class UnionAndIntersection {
 	 * */
 	//Overall time complexity = O(max(nlogn, mlogm))
 	
-	public Node head1;
-	public Node head2;
+	public static Node head1;
+	public static Node head2;
 	public static class Node {
 		int data;
 		Node next;
@@ -60,33 +61,8 @@ public class UnionAndIntersection {
 		}
 	}
 	
-	//add node to the first linked list
-	public void addToFirstLinkedList(Node node) {
-		if(head1 == null) {
-			head1 = node;
-		} else {
-			Node temp = head1;
-			while(temp.next != null) {
-				temp = temp.next;
-			}
-			temp.next = node;
-		}
-	}
-	//add node to the second linked list
-	public void addToSecondLinkedList(Node node) {
-		if(head2 == null) {
-			head2 = node;
-		} else {
-			Node temp = head2;
-			while(temp.next != null) {
-				temp = temp.next;
-			}
-			temp.next = node;
-		}
-	}
-	
-	//print the list
-	public void print(Node node) {
+	//display node data
+	public static void print(Node node) {
 		Node temp = node;
 		while(temp != null) {
 			System.out.println(temp.data);
@@ -94,62 +70,177 @@ public class UnionAndIntersection {
 		}
 	}
 	
-	//remove the duplicates from first linked list
-	public void removeDuplicateFromFirstList() {
-		Set<Integer> set = new HashSet<>();
-		Node current = head1;
+	//remove duplicates from the list
+	public static void removeDuplicates(Node node) {
+		Node current = node;
 		Node previous = null;
+		Set<Integer> set = new HashSet<>();
+		
 		while(current != null) {
-			//if current value is already in the set
 			if(set.contains(current.data)) {
 				previous.next = current.next;
 			} else {
 				set.add(current.data);
 				previous = current;
 			}
-			current = current.next;
+			current = previous.next;
 		}
-		print(head1);
 	}
 	
-	//remove the duplicates from second linked list
-	public void removeDuplicateFromSecondList() {
-		Set<Integer> set = new HashSet<>();
-		Node current = head2;
-		Node previous = null;
-		while(current != null) {
-			//if current value is already in the set
-			if(set.contains(current.data)) {
-				previous.next = current.next;
-			} else {
-				set.add(current.data);
-				previous = current;
-			}
-			current = current.next;
+	//sort the two linked list (merge sort)
+	public static Node mergeSort(Node node) {
+		if (node == null || node.next == null) {
+			return node;
 		}
-		print(head2);
+		Node middle = getMiddle(node);
+		Node newList = middle.next;
+		middle.next = null;
+		//apply merge sort to the left and right list
+		Node left = mergeSort(node);
+		Node right = mergeSort(newList);
+		//merge both the left and right list
+		return merge(left, right);
+	}
+	
+	//merge two list
+	public static Node merge(Node left, Node right) {
+		if(left == null)
+			return right;
+		if(right == null)
+			return left;
+
+		Node mergedNode = null;
+		if(left.data <= right.data) {
+			mergedNode = left;
+			mergedNode.next = merge(left.next, right);
+		} else {
+			mergedNode = right;
+			mergedNode.next = merge(left, right.next);
+		}
+		return mergedNode;
+	}
+	
+	//get the middle of the node
+	public static Node getMiddle(Node node) {
+		Node temp1 = node;
+		Node temp2 = node;
+		while(temp2 != null && temp2.next != null && temp2.next.next != null) {
+			temp1 = temp1.next;
+			temp2 = temp2.next.next;
+		}
+		return temp1;
+	}
+	
+	//get the union of two list
+	public static Node getUnion(Node list1, Node list2) {
+		if(list1 == null && list2 == null)
+			return null;
+		Node union = new Node(0);
+		Node tempUnion = union;
+		while(list1.next != null && list2.next != null) {
+			if(list1.data < list2.data) {
+				tempUnion.data = list1.data;
+				list1 = list1.next;
+			} else if(list2.data < list1.data) {
+				tempUnion.data = list2.data;
+				list2 = list2.next;
+			} else {
+				tempUnion.data = list2.data;
+				list1 = list1.next;
+				list2 = list2.next;
+			}
+			if(list1 != null && list2 != null)
+				tempUnion = tempUnion.next = new Node(0);
+		}
+		if(list1.next != null) {
+			while(list1 != null) {
+				tempUnion.data = list1.data;
+				list1 = list1.next;
+				if(list1 != null)
+					tempUnion = tempUnion.next = new Node(0);
+			}
+		}
+		if(list2.next != null) {
+			while(list2 != null) {
+				tempUnion.data = list2.data;
+				list2 = list2.next;
+				if(list2 != null)
+					tempUnion = tempUnion.next = new Node(0);
+			}
+		}
+		return union;
+	}
+	
+	//get the intersection of two lists
+	public static Node getIntersection(Node list1, Node list2) {
+		if(list1 == null && list2 == null)
+			return null;
+		Node intersection = null;
+		Node tempIntersection = null;
+		while(list1 != null && list2 != null) {
+			if(list1.data < list2.data) {
+				list1 = list1.next;
+			} else if(list2.data < list1.data) {
+				list2 = list2.next;
+			} else {
+				if(intersection == null) {
+					intersection = new Node(0);
+					tempIntersection = intersection;
+				}
+				else {
+					tempIntersection = tempIntersection.next = new Node(0);
+				}
+				tempIntersection.data = list2.data;
+				list1 = list1.next;
+				list2 = list2.next;
+			}
+		}
+		return intersection;
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		UnionAndIntersection obj1 = new UnionAndIntersection();
-		UnionAndIntersection obj2 = new UnionAndIntersection();
+		head1 = new Node(1);
+		head1.next = new Node(2);
+		head1.next.next = new Node(5);
+		head1.next.next.next = new Node(4);
+		head1.next.next.next.next = new Node(3);
+		head1.next.next.next.next.next = new Node(3);
+		head2 = new Node(3);
+		head2.next = new Node(8);
+		head2.next.next = new Node(5);
+		head2.next.next.next = new Node(4);
+		head2.next.next.next.next = new Node(9);
+		head2.next.next.next.next.next = new Node(3);
+		head2.next.next.next.next.next.next = new Node(7);
+		head2.next.next.next.next.next.next.next = new Node(6);
 		
-		obj1.addToFirstLinkedList(new Node(1));
-		obj1.addToFirstLinkedList(new Node(3));
-		obj1.addToFirstLinkedList(new Node(2));
-		obj1.addToFirstLinkedList(new Node(5));
-		obj1.addToFirstLinkedList(new Node(4));
-		
-		obj2.addToSecondLinkedList(new Node(2));
-		obj2.addToSecondLinkedList(new Node(2));
-		obj2.addToSecondLinkedList(new Node(4));
-		obj2.addToSecondLinkedList(new Node(5));
-		obj2.addToSecondLinkedList(new Node(1));
-		obj2.addToSecondLinkedList(new Node(6));
-		obj2.addToSecondLinkedList(new Node(6));
-		obj1.removeDuplicateFromFirstList();
-		obj2.removeDuplicateFromSecondList();
+		System.out.println("----------initially head1----------");
+		print(head1);
+		System.out.println("----------initially head2----------");
+		print(head2);
+		//remove the duplicates of head1 and head2 list
+		removeDuplicates(head1);
+		removeDuplicates(head2);
+		System.out.println("-----After removing the duplicates-----");
+		System.out.println("----------head1----------");
+		print(head1);
+		System.out.println("----------head2----------");
+		print(head2);
+		//sort head1 and head2
+		head1 = mergeSort(head1);
+		head2 = mergeSort(head2);
+		System.out.println("-----After sorting-----");
+		System.out.println("----------head1----------");
+		print(head1);
+		System.out.println("----------head2----------");
+		print(head2);
+		Node union = getUnion(head1, head2);
+		System.out.println("----------Union----------");
+		print(union);
+		Node intersection = getIntersection(head1, head2);
+		System.out.println("----------Intersection----------");
+		print(intersection);
 	}
 
 }
